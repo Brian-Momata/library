ca2c731628e7cd6e1425564ffca707f0287963ab
Brian-Momata <107246922+Brian-Momata@users.noreply.github.com>
2023-08-18 11:02:48 +0300

    methods to add Book object

--- script.js
+++ script.js
@@ -11,3 +11,33 @@ function Book(title, author, pages, read = false) {
     (this.read ? " Already read." : " Not read yet.");
   }
 }
+
+const addBookButton = document.getElementById("add-book-button");
+
+addBookButton.addEventListener('click', (event) => {
+  addBook();
+  resetForm();
+  event.preventDefault(); // prevent the form from submitting
+});
+
+function addBook(){
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const pages = parseInt(document.getElementById('pages').value);
+  const read = document.getElementById('read').checked;
+
+  const newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+}
+
+function resetForm(){
+  const title = document.getElementById('title');
+  const author = document.getElementById('author');
+  const pages = document.getElementById('pages');
+  const read = document.getElementById('read');
+
+  title.value = '';
+  author.value = '';
+  pages.value = '';
+  read.checked = false;
+}
