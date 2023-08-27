8c143584ca57da2f20bec3a7915fb4bfdb79762d
Brian-Momata <107246922+Brian-Momata@users.noreply.github.com>
2023-08-27 10:46:11 +0300

    Hoist variable declarations to the top

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
+// Define the main library array
 const myLibrary = [];
 
 // Get the container for the book list
 const bookListContainer = document.getElementById('book-list');
+
+// Get the add book form
+const addBookForm = document.getElementById('add-book-form');
+
+// Handling the dialog for adding the book
+const openDialogButton = document.getElementById('open-dialog-button');
+const dialog = document.getElementById('add-book-dialog');
+const closeDialogButton = document.getElementById('close-dialog-button');
 
 // Constructor function for Book objects
 function Book(title, author, pages, read = false) {
@@ -10,33 +19,21 @@ function Book(title, author, pages, read = false) {
   this.pages = pages;
   this.read = read;
   this.info = () => {
-    return (
-      "" +
-      this.title +
-      " by " +
-      this.author +
-      ", " +
-      this.pages +
-      " pages." +
-      (this.read ? " Already read." : " Not read yet.")
-    );
+    return `${this.title} by ${this.author}, ${this.pages} pages. ${this.read ? 'Already read.' : 'Not read yet.'}`;
   };
 }
 
-// Get the add book form
-const addBookForm = document.getElementById("add-book-form");
-
 // Event listener for form submission
 addBookForm.addEventListener('submit', (event) => {
+  event.preventDefault();
   addBook();
   resetForm();
-  event.preventDefault();
   dialog.close();
   renderBooks();
 });
 
 // Function to add a book to the library
-function addBook(){
+function addBook() {
   const title = document.getElementById('title').value;
   const author = document.getElementById('author').value;
   const pages = parseInt(document.getElementById('pages').value);
@@ -47,16 +44,12 @@ function addBook(){
 }
 
 // Function to reset the form
-function resetForm(){
-  const title = document.getElementById('title');
-  const author = document.getElementById('author');
-  const pages = document.getElementById('pages');
-  const read = document.getElementById('read');
+function resetForm() {
+  const formElements = ['title', 'author', 'pages', 'read'];
 
-  title.value = '';
-  author.value = '';
-  pages.value = '';
-  read.checked = false;
+  formElements.forEach((elementId) => {
+    document.getElementById(elementId).value = '';
+  });
 }
 
 // Function to render books in the book list container
@@ -66,7 +59,7 @@ const renderBooks = () => {
   myLibrary.forEach((book, index) => {
     const bookItem = document.createElement('div');
     bookItem.classList.add('book');
-    bookItem.setAttribute('data-index', index); // Set data-index attribute
+    bookItem.setAttribute('data-index', index);
     bookItem.innerHTML = `
       <h3>${book.title}</h3>
       <p>Author: ${book.author}</p>
@@ -78,20 +71,20 @@ const renderBooks = () => {
 
     bookListContainer.appendChild(bookItem);
   });
-}
+};
 
 // Function to navigate to the book details page
-function navigateToBook(book){
+function navigateToBook(book) {
   const libraryData = JSON.stringify(myLibrary);
   const bookPageIndex = `book_details.html?index=${myLibrary.indexOf(book)}&library=${encodeURIComponent(libraryData)}`;
   window.location.href = bookPageIndex;
 }
 
 // Function to update library based on URL parameters
-function updateLibray(){
+function updateLibrary() {
   const currentURL = window.location.href;
 
-  if (currentURL !== 'http://127.0.0.1:5500/index.html'){
+  if (currentURL !== 'http://127.0.0.1:5500/index.html') {
     const urlParams = new URLSearchParams(window.location.search);
     const newLibArray = JSON.parse(decodeURIComponent(urlParams.get('library')));
 
@@ -99,17 +92,15 @@ function updateLibray(){
     renderBooks();
   }
 }
-updateLibray();
 
-// Handling the dialog for adding the book
-const openDialogButton = document.getElementById("open-dialog-button");
-const dialog = document.getElementById("add-book-dialog");
-const closeDialogButton = document.getElementById("close-dialog-button");
+// Initial library update
+updateLibrary();
 
-openDialogButton.addEventListener("click", function () {
+// Event listeners for the dialog
+openDialogButton.addEventListener('click', () => {
   dialog.showModal();
 });
 
-closeDialogButton.addEventListener("click", function () {
+closeDialogButton.addEventListener('click', () => {
   dialog.close();
 });
--- book_details.js
+++ book_details.js
@@ -11,21 +11,8 @@ const bookIndex = urlParams.get('index');
 // Set the book variable
 const book = myLibrary[parseInt(bookIndex)];
 
-if (bookIndex !== null) {
-  displayBookDetails(book);
-}
-
-// Function to display book details
-function displayBookDetails(book){
-  const bookDetailsContainer = document.getElementById('book-details');
-  bookDetailsContainer.innerHTML = `
-    <h2>${book.title}</h2>
-    <p>Author: ${book.author}</p>
-    <p>Pages: ${book.pages}</p>
-    <p>${book.read ? 'Already read' : 'Not read yet'}</p>
-  `;
-}
-
+// Get the book details container
+const bookDetailsContainer = document.getElementById('book-details');
 const deleteButton = document.getElementById('delete-book');
 const toggleReadButton = document.getElementById('toggle-read');
 
@@ -41,7 +28,17 @@ toggleReadButton.addEventListener('click', () => {
   updateURLParams();
 });
 
-// Function to update URL parameters
+// Function to display book details
+function displayBookDetails(book) {
+  bookDetailsContainer.innerHTML = `
+    <h2>${book.title}</h2>
+    <p>Author: ${book.author}</p>
+    <p>Pages: ${book.pages}</p>
+    <p>${book.read ? 'Already read' : 'Not read yet'}</p>
+  `;
+}
+
+// Function to update URL parameters to reflect changes to book's read status
 function updateURLParams() {
   const libraryData = JSON.stringify(myLibrary);
   const newURL = `book_details.html?index=${bookIndex}&library=${encodeURIComponent(libraryData)}`;
@@ -49,9 +46,14 @@ function updateURLParams() {
 }
 
 // Function to delete a book
-function deleteBook(){
+function deleteBook() {
   myLibrary.splice(bookIndex, 1);
   const libraryData = JSON.stringify(myLibrary);
   const homePage = `index.html?library=${encodeURIComponent(libraryData)}`;
   window.location.href = homePage;
+}
+
+// Check if book index is not null and display book details
+if (bookIndex !== null) {
+  displayBookDetails(book);
 }
